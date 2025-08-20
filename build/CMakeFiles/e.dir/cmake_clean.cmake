505806ae6b113e2847a5f95ed20f2b367f8f05ea
MovingJu <baramishe@gmail.com>
2025-08-21 01:05:00 +0900

    v0.2

---
+++ build/CMakeFiles/e.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/e.dir/link.d"
+  "CMakeFiles/e.dir/src/main.cpp.o"
+  "CMakeFiles/e.dir/src/main.cpp.o.d"
+  "e"
+  "e.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/e.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
